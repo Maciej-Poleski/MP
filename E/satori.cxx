05178dd4f54dacd738ea92486dbf8bbb5bdd66c0
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2012-03-15 15:35:08 +0100

    Zadanie E przepchnięte

--- E/satori.cxx
+++ E/satori.cxx
@@ -29,7 +29,13 @@ namespace Wrapper
 {
 std::istream &in = std::cin;
 std::ostream &out = std::cout;
+}
+#endif
 
+#include <tr1/cstdint>
+
+namespace Wrapper
+{
 typedef std::tr1::uint_fast64_t uint_fast64_t;
 typedef std::tr1::uint_fast32_t uint_fast32_t;
 typedef std::tr1::uint_fast16_t uint_fast16_t;
@@ -39,17 +45,94 @@ typedef std::tr1::uint64_t uint64_t;
 typedef std::tr1::uint32_t uint32_t;
 typedef std::tr1::uint16_t uint16_t;
 typedef std::tr1::uint8_t uint8_t;
-
 }
-#endif
 
-#include <tr1/cstdint>
 #include <string>
 #include <algorithm>
 
-Wrapper::uint32_t *INORDER = 0;
-Wrapper::uint32_t *PREORDER = 0;
-Wrapper::uint32_t *POSTORDER = 0;
+static Wrapper::uint32_t *INORDER = 0;
+static Wrapper::uint32_t *PREORDER = 0;
+static Wrapper::uint32_t *POSTORDER = 0;
+static Wrapper::uint32_t *inPtr = 0;
+
+static inline void makePostorder(const Wrapper::uint32_t preBegin,
+                                 const Wrapper::uint32_t inBegin,
+                                 const Wrapper::uint32_t length)
+{
+    if(length == 1)
+        Wrapper::out << INORDER[inBegin] << ' ';
+    else if(length > 1) {
+        Wrapper::uint32_t p;
+        for(Wrapper::uint32_t i = 0;; ++i) {
+            if((i % 2) == 1 && INORDER[inBegin + length - 1 - (i / 2)] == PREORDER[preBegin]) {
+                p = inBegin + length - 1 - (i / 2);
+                break;
+            } else if((i % 2) == 0 && INORDER[inBegin + (i / 2)] == PREORDER[preBegin]) {
+                p = inBegin + (i / 2);
+                break;
+            }
+        }
+        Wrapper::uint32_t leftLength = p - inBegin;
+        makePostorder(preBegin + 1, inBegin, leftLength);
+        makePostorder(preBegin + 1 + leftLength, inBegin + leftLength + 1, length - 1 - leftLength);
+        Wrapper::out << INORDER[p] << ' ';
+    }
+}
+
+static inline void makePreorder(const Wrapper::uint32_t postBegin,
+                                const Wrapper::uint32_t inBegin,
+                                const Wrapper::uint32_t length)
+{
+    if(length == 1)
+        Wrapper::out << INORDER[inBegin] << ' ';
+    else if(length > 1) {
+        Wrapper::uint32_t p;
+        for(Wrapper::uint32_t i = 0;; ++i) {
+            if((i % 2) == 1 && INORDER[inBegin + length - 1 - (i / 2)] == POSTORDER[postBegin + length - 1]) {
+                p = inBegin + length - 1 - (i / 2);
+                break;
+            } else if((i % 2) == 0 && INORDER[inBegin + (i / 2)] == POSTORDER[postBegin + length - 1]) {
+                p = inBegin + (i / 2);
+                break;
+            }
+        }
+        Wrapper::uint32_t leftLength = p - inBegin;
+        Wrapper::out << INORDER[p] << ' ';
+        makePreorder(postBegin, inBegin, leftLength);
+        makePreorder(postBegin + leftLength, inBegin + leftLength + 1, length - 1 - leftLength);
+    }
+}
+
+static inline bool makeInorder(const Wrapper::uint32_t preBegin,
+                               const Wrapper::uint32_t postBegin,
+                               const Wrapper::uint32_t length)
+{
+    if(length == 0)
+        return true;
+    else if(length == 1) {
+        *inPtr++ = PREORDER[preBegin];
+        return true;
+    } else {
+        check(PREORDER[preBegin]==POSTORDER[postBegin+length-1]);
+        if(PREORDER[preBegin + 1] == POSTORDER[postBegin + length - 1 - 1])
+            return false;
+        Wrapper::uint32_t p;
+        for(Wrapper::uint32_t i = 0;; ++i) {
+            if((i % 2) == 1 && POSTORDER[postBegin + length - 1 - 1 - 1 - (i / 2)] == PREORDER[preBegin + 1]) {
+                p = postBegin + length - 1 - 1 - 1 - (i / 2);
+                break;
+            } else if((i % 2) == 0 && POSTORDER[postBegin + (i / 2)] == PREORDER[preBegin + 1]) {
+                p = postBegin + (i / 2);
+                break;
+            }
+        }
+        Wrapper::uint32_t leftLength = p - postBegin+1;
+        if(!makeInorder(preBegin + 1, postBegin, leftLength))
+            return false;
+        *inPtr++ = PREORDER[preBegin];
+        return makeInorder(preBegin + 1 + leftLength, postBegin + leftLength, length - 1 - leftLength);
+    }
+}
 
 inline static void solution()
 {
@@ -59,12 +142,11 @@ inline static void solution()
     while(z--) {
         std::tr1::uint_fast32_t n;
         in >> n;
-        while(n--) {
+        bool havePre = false;
+        bool havePost = false;
+        bool haveIn = false;
+        for(std::tr1::uint_fast8_t q = 0; q < 2; ++q) {
             std::string order;
-            bool havePre = false;
-            bool havePost = false;
-            bool haveIn = false;
-
             in >> order;
             if(order[1] == 'R') {
                 havePre = true;
@@ -89,19 +171,23 @@ inline static void solution()
                     in >> *i;
                 }
             }
-            if(!havePre)
-	    {
-		// make preorder
-	    }
-	    else if(!havePost)
-	    {
-		// make postorder
-	    }
-	    else
-	    {
-		check(!haveIn);
-		// make inorder
-	    }
+        }
+        if(!havePre) {
+            makePreorder(0, 0, n);
+            out << '\n';
+        } else if(!havePost) {
+            makePostorder(0, 0, n);
+            out << '\n';
+        } else {
+            check(!haveIn);
+            delete [] INORDER;
+            inPtr = INORDER = new Wrapper::uint32_t[n];
+            if(makeInorder(0, 0, n)) {
+                for(uint32_t *i = INORDER; i != inPtr; ++i)
+                    out << *i << ' ';
+                out << '\n';
+            } else
+                out << "ERROR\n";
         }
     }
 }
